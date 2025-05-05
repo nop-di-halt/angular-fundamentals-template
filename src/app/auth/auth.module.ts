084e9c8755d179de7e748a0b4633e95ecdb814f0
Dmytro <manicwheelman@yahoo.com>
2025-05-05 05:21:28 +0300

    bs3

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '@app/shared/shared.module';
 import { TokenInterceptor } from './interceptors/token.interceptor';
+import { AuthService } from './services/auth.service';
+import { SessionStorageService } from './services/session-storage.service';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { TokenInterceptor } from './interceptors/token.interceptor';
     HttpClientModule,
     SharedModule
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }]
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }, SessionStorageService,
+    AuthService,
+]
 })
 export class AuthModule { }
